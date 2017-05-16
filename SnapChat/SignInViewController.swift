1224cea5e222d8d9dff79df5059046a9abef87df
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-05-17 01:48:36 +0200

    logout added (lecture 103)

--- SnapChat/SignInViewController.swift
+++ SnapChat/SignInViewController.swift
@@ -11,25 +11,39 @@ import Firebase
 import FirebaseAuth
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
-
-  
+    
+    
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
- 
+    
     @IBAction func turnUpTapped(_ sender: Any) {
         
         FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
             print("We tried to Sign In")
             if error != nil {
                 print("Hey we have an error:\(error)")
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    
+                    if error != nil {
+                        print("Hey we have an error:\(error)")
+                    } else {
+                        print("Created user successfully")
+                        self.performSegue(withIdentifier: "signinsegue", sender: nil)
+                    }
+                })
+                
             } else {
                 print("Signed In Successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender: nil)
+                
             }
         })
         
---
+++ SnapChat/SnapsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SnapsViewController.swift
+//  SnapChat
+//
+//  Created by Andrzej Palec on 17/05/2017.
+//  Copyright © 2017 Andrzej Palec. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
