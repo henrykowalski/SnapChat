e71b97cc7b71006035e4b1644ec5605bbc250af0
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-05-17 00:19:57 +0200

    pred programovanim SignIn

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+platform :ios, '10.3'
+platform :ios
+
+target 'SnapChat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SnapChat
+
+# puvodne tam byl radek prvni nasledujici: pod 'Firebase/Core' /// a tohle uplne chybelo:  pod 'Firebase'
+# pod 'Firebase/Core'
+
+pod 'Firebase'
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Storage'
+
+end
---
+++ SnapChat/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  SnapChat
+//
+//  Created by Andrzej Palec on 07/05/2017.
+//  Copyright © 2017 Andrzej Palec. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UIStackView!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+ 
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
+    }
+    
+}
+
