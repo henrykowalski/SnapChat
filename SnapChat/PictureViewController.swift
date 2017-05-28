8358f1dcf2fc4ae189674cee7fa7a4cdc01863dd
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-05-28 17:28:21 +0200

    FIREBASE STORAGE / UKLADAME OBRAZEK DO IMAGES zacatek

--- SnapChat/PictureViewController.swift
+++ SnapChat/PictureViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
-class PictureViewController: UIViewController {
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
@@ -16,10 +18,27 @@ class PictureViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imageView.backgroundColor = UIColor.clear
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +47,33 @@ class PictureViewController: UIViewController {
     }
     
     @IBAction func cametaTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    
+        
     }
 
     @IBAction func nextTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "selectUsersegue", sender: nil)
+        
     }
-}
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let imagesFolder = FIRStorage.storage().reference().child("images")
+        
+        let imageData = UIImagePNGRepresentation(imageView.image!)!
+        
+        imagesFolder.child("images.png").put(imageData, metadata: nil, completion: {(metadata, error) in
+            print("We tried to upload!")
+            if error != nil {
+                print("We had an error:\(error)")
+            }
+            })
+        }
+        
+    }
